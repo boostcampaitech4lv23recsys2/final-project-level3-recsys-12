6c02e1383d6492f9997e7feeefab71f6f5d3ed20
KJH <tree.jhk@gmail.com>
2023-01-24 10:14:09 +0000

    Feat: Add inference at back-end

--- backend/inference/predict.py
+++ backend/inference/predict.py
@@ -1,25 +1,62 @@
+import sys
+import torch
+import random
+
+sys.path.append("../model/AE_model/")
+from Multi_DAE import *
+from utils import *
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
 class Model:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, model_info, df) -> None:
+        self.model_path = model_info["path"]["model_path"]
+        self.p_dims = model_info["parameter"]["p_dims"]
+        self.topk = model_info["inference"]["topk"]
+        self.model = MultiDAE(
+                p_dims=self.p_dims + [df.item.nunique()],
+            ).to(device)
+        self.model.load_state_dict(torch.load(self.model_path))
+        self.device = device
+        self.house_encoder, self.house_decoder = generate_encoder_decoder(df, "house")
+        self.item_encoder, self.item_decoder = generate_encoder_decoder(df, "item")
+        self.dummy_input = torch.zeros((df.item.nunique()), dtype=torch.int64)
 
     def predict(self, data):
         return self.forward(data)
 
     # item_id, 가구명, 가구파는 곳, 가격, 이미지 url
-    def forward(self, data):
-        '''
-        data: 유저가 선택한 선호하는 집들이의 가구들
-        '''
-        # data_dict = {'item' : {1,4,5,6,8,9},
-        # 'furniture_name' : {'a','b','c','d','e'},
-        # 'seller' : {'IKEA','LG','SamSung','Ohou','Hansam'},
-        # 'price' : {100,100,100,100,100,100},
-        # "image_url" : {'a/','b/','c/','d/','e/'},}
-        item_list = [1, 4, 5, 6, 8, 9, 10, 15, 16, 18, 19]
+    def forward(self, user_selected_data):
+        self.model.eval()
         
-        return item_list
+        # 원래는 아래 코드가 잘 동작해야 합니다
+        # user_selected_data = torch.tensor(user_selected_data).apply_(lambda x: self.item_encoder[x])
+        
+        # 하지만, DB에서 저장된 item_id와 csv 파일에서의 item_id 간의 차이가 있어서인지
+        # csv 파일에서의 item_id로 만들어진 self.item_encoder가, DB에서 불러온 item_id 간 차이로 key error 발생합니다
+        tmp = []
+        for item_id in user_selected_data:
+            try:
+                tmp.append(self.item_encoder[item_id])
+            except:
+                pass
+        # 만약 tmp가 빈 리스트라면, tmp는 item_encoder에 key값으로 포함된 item들
+        if not tmp: tmp = random.sample(list(self.item_decoder.keys()), k=50)
+        user_selected_data = torch.tensor(tmp)
+        tmp = user_selected_data[:]
+        user_selected_data = self.dummy_input[:]
+        user_selected_data[tmp] = 1
+        user_selected_data = user_selected_data.unsqueeze(dim=0)
 
-MODEL = Model() # TODO: model load 하는 코드로 변경
+        with torch.no_grad():
+            prediction = []
+            model_output = self.model(user_selected_data.type(torch.FloatTensor).to(self.device))
+            recommend_res = model_output.argsort(dim=1).squeeze()
+            up = recommend_res[-self.topk:].cpu().numpy().tolist()
+            for item in up[::-1]:
+                prediction.append(self.item_decoder[item])
+        return prediction
 
-def inference(data):
+def inference(data, MODEL):
     return MODEL.predict(data)
---
+++ model/AE_model/dataloader.py
@@ -0,0 +1,119 @@
+import os
+import warnings
+from collections import defaultdict
+
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+
+warnings.filterwarnings(action="ignore")
+torch.set_printoptions(sci_mode=True)
+
+import sys
+from utils import *
+
+
+class MakeMatrixDataSet:
+    """
+    MatrixDataSet 생성
+    """
+
+    def __init__(self, args):
+        self.config = args
+        self.df = pd.read_csv(os.path.join(self.config.data_path, "train_v2.csv")).groupby("house").filter(lambda x: len(x) >= 15)
+        self.item_encoder, self.item_decoder = self.generate_encoder_decoder("item")
+        self.user_encoder, self.user_decoder = self.generate_encoder_decoder("house")
+        self.num_item, self.num_user = len(self.item_encoder), len(self.user_encoder)
+
+        self.df["item_idx"] = self.df["item"].apply(lambda x: self.item_encoder[x])
+        self.df["house_idx"] = self.df["house"].apply(lambda x: self.user_encoder[x])
+
+        self.user_train, self.user_valid = self.generate_sequence_data()
+
+    def generate_encoder_decoder(self, col: str) -> dict:
+        """
+        encoder, decoder 생성
+
+        Args:
+            col (str): 생성할 columns 명
+        Returns:
+            dict: 생성된 user encoder, decoder
+        """
+
+        encoder = {}
+        decoder = {}
+        ids = self.df[col].unique()
+
+        for idx, _id in enumerate(ids):
+            encoder[_id] = idx
+            decoder[idx] = _id
+
+        return encoder, decoder
+
+    def generate_sequence_data(self) -> dict:
+        """
+        sequence_data 생성
+
+        Returns:
+            dict: train user sequence / valid user sequence
+        """
+        users = defaultdict(list)
+        user_train = {}
+        user_valid = {}
+        for user, item in zip(
+            self.df["house_idx"], self.df["item_idx"]
+        ):
+            users[user].append(item)
+
+        for user in users:
+            np.random.seed(self.config.seed)
+
+            user_total = users[user]
+            valid = np.random.choice(
+                user_total, size=self.config.valid_samples, replace=False
+            ).tolist()
+            train = list(set(user_total) - set(valid))
+
+            user_train[user] = train
+            user_valid[user] = valid  # valid_samples 개수 만큼 검증에 활용 (현재 Task와 가장 유사하게)
+
+        return user_train, user_valid
+
+    def get_train_valid_data(self):
+        return self.user_train, self.user_valid
+
+    def make_matrix(self, user_list, train=True):
+        """
+        user_item_dict를 바탕으로 행렬 생성
+        """
+        mat = torch.zeros(size=(user_list.size(0), self.num_item))
+        for idx, user in enumerate(user_list):
+            if train:
+                mat[idx, self.user_train[user.item()]] = 1
+            else:
+                mat[
+                    idx, self.user_train[user.item()] + self.user_valid[user.item()]
+                ] = 1
+        return mat
+
+
+class AEDataSet(Dataset):
+    def __init__(self, num_user):
+        self.num_user = num_user
+        self.users = [i for i in range(num_user)]
+
+    def __len__(self):
+        return self.num_user
+
+    def __getitem__(self, idx):
+        user = self.users[idx]
+        return torch.LongTensor([user])
+
+def get_inference_data(args: object, generate_encoder_decoder: object):
+    df = pd.read_csv(os.path.join(args.data_path, "train_v2.csv")).groupby("house").filter(lambda x: len(x) >= 15)
+    house_encoder, house_decoder = generate_encoder_decoder(df, "house")
+    item_encoder, item_decoder = generate_encoder_decoder(df, "item")
+    dummy_data = make_single_dummy_user_input(df, item_encoder, n_interaction = 50)
+    
+    return dummy_data, item_encoder, item_decoder, house_encoder, house_decoder
